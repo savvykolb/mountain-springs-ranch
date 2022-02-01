d0ead614a029632a667008c3178fa6853d7cfbde
Savannah Kolb <savannahnkolb@gmail.com>
2022-02-01 12:21:34 -0500

    past pup page working

--- src/components/pages/PastPup.js
+++ src/components/pages/PastPup.js
@@ -1,39 +1,33 @@
+
 import '../style/style.css';
-import { puppies } from "./puppies"
-
-
+import { puppies } from "./puppies";
+import { Card, Row, Col, Container } from "react-bootstrap";
 
 function PastPup() {
     return (
-
         <section id="portfolio" class="portfolio">
             <div className="container">
                 <h4 className="d-flex justify-content-center">
-                    These are just a few of past and present puppies. You can arrange a time and date to come visit any available puppies. 
+                    These are just a few of the past and present puppies. You can arrange a time and date to come visit any available puppies.
                 </h4>
                 <br></br>
-
-              {puppies.map((puppy, index) => (
-                    <div key={index}>
-                        <div className="row portfolio-container">
-                            <div className="col-lg-4 col-md-6 portfolio-item filter-app">
-                                <div className="portfolio-wrap">
-                                    <img className="img-fluid" alt="Puppy" src= {puppy.image} />
-                                    <div className="portfolio-info">
-                                        <h2>{puppy.year}</h2>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )
-                )}
-
-
+                <Container>
+                    <Row >
+                        {puppies.map((puppy, index) => (
+                            <Col className="portfolio" key={index} xs={12} md={4} lg={3}>
+                                <Card className="portfolio-wrap">
+                                    <Card.Img className="img" src={puppy.image} />
+                                    <Card.Body className="portfolio-info">
+                                        <Card.Title>{puppy.year}</Card.Title>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        )
+                        )}
+                    </Row>
+                </Container>
             </div>
         </section>
-
-
     );
 }
 
