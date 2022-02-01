3b8a5afd64c86b22591e4a76d233d1ddf573f78b
Savannah Kolb <savannahnkolb@gmail.com>
2022-02-01 17:06:47 -0500

    started contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,35 @@
 import '../style/style.css';
+import { BsFillEnvelopeOpenFill, BsPhone } from "react-icons/bs"
 
 function Contact() {
     return (
-        <div>
-            <h1>Contact Page</h1>
-        </div>
+        <section>
+            <div class="container contact">
+                <div className='info'>
+
+                    <div class="title">
+                        Contact
+                    </div>
+                    <h5 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', }} className="d-flex">
+                        Interested in becoming apart of the basenji family?
+                    </h5>
+                    <br></br>
+                    <br></br>
+                    <div class="social-links">
+                        <div className="phone">
+                            <a target='_blank' rel="noreferrer" href="tel:951-763-4672" class="phoneNumber"><BsPhone /></a>
+                            <h4>Call:</h4>
+                            <p>+1 951-763-4672</p>
+                        </div>
+                        <div className="email">
+                        <a target='_blank' rel="noreferrer" href="#" class="email"><BsFillEnvelopeOpenFill /></a>
+                        <h4>Email:</h4>
+                            <p>TBD</p>
+                        </div>
+                    </div>
+                </div>
+            </div >
+        </section >
     );
 }
 
