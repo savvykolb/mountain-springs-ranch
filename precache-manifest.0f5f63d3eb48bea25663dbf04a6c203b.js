1ad235bdfbaacf324615acc93030255bc3ef77c2
Savannah Kolb <savannahnkolb@gmail.com>
2023-01-14 23:29:51 -0500

    Updates

---
+++ precache-manifest.0f5f63d3eb48bea25663dbf04a6c203b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd15b94ff1c4849606006b54bebcd2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "200bb1c89ee5d1365344",
+    "url": "/static/css/2.366b4564.chunk.css"
+  },
+  {
+    "revision": "e356b98a9eacab06a865",
+    "url": "/static/css/main.f5b6d957.chunk.css"
+  },
+  {
+    "revision": "200bb1c89ee5d1365344",
+    "url": "/static/js/2.6b60f57b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6b60f57b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e356b98a9eacab06a865",
+    "url": "/static/js/main.32b6d5c1.chunk.js"
+  },
+  {
+    "revision": "173026e76cb3d4d06430",
+    "url": "/static/js/runtime-main.213fee62.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
